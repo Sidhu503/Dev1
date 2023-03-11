27eb401cb6a7454cc7094cfebaaafb5a8f93fa3c
Sidhu503 <sudharshansidhu456@gmail.com>
2023-03-11 12:20:25 +0530

    Substraction

--- 1.java
+++ 1.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         int a=96;
         int b=66;
-        System.out.println("Hello,world");
-        System.out.println(a+b); 
+        System.out.println("Hello,world"); 
+        System.out.println(a-b);
     }
 }
