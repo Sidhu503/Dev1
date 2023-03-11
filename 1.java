532351c1189567e4c924c5f4807bf60a8c51956b
Sidhu503 <sudharshansidhu456@gmail.com>
2023-03-11 12:08:42 +0530

    Addition

--- 1.java
+++ 1.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        int a=96;
+        int b=66;
+        System.out.println("Hello,world");
+        System.out.println(a+b); 
     }
 }
